400cf61ba0f7c4427afa35fb37b6d0f75f532c7b
azatyanviola <viola.azatyan@mail.ru>
2022-04-29 12:22:21 +0300

    Fint the number of points

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 'use string'
 
 
-console.log('The breakfast is at 09:00 in the room at 123:456'.match( /\b\d\d:\d\d\b/ ) ); // 9:00
+console.log('The breakfast is at 09:00 in the room at 123:456'.match( /\b\d\d:\d\d\b/ ) ); // 09:00
 
 //********************** */
 
 let regexp = /\d\d[-:]\d\d/g;
 
-console.log('The breakfast will be at 09:00 and the dinner will be at 15-00'.match(regexp));
+console.log('The breakfast will be at 09:00 and the dinner will be at 15-00'.match(regexp)); // 09:00  15-00
+
+//*************** */
+
+let regexp2 = /\.{3,}/g;
+
+console.log('Hello!... , How are you?.....'.match(regexp2)); // ...,   .....
+
+//*************** */
